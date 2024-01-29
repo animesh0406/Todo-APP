f1cc2de566928738980a1ffc0e913c3ac04603ec
Animesh Sharma <animesh.sharma0406@gmail.com>
2024-01-29 14:28:43 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
   }
 
   const toggleComplete=(id)=>{
-    setTodos((prev)=> prev.map((prevTodo)=>(prevTodo.id===id?{...prevTodo,completed:!prevTode.completed}:prevTodo)))
+    setTodos((prev)=> prev.map((prevTodo)=>(prevTodo.id===id?{...prevTodo,completed:!prevTodo.completed}:prevTodo)))
   }
 
    useEffect(() => {
